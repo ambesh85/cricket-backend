c090cc7abdd34b2b686278341aacf8d02a2c2520
Navneet8586 <machinelearning8586@gmail.com>
2025-12-27 11:41:54 +0530

    Fix backend and user-react apps

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 console.log("✅ BACKEND INDEX.JS LOADED");
 
 /* ================== IMPORTS ================== */
@@ -26,7 +28,6 @@ app.use(express.json());
 /* ================== MONGODB ================== */
 mongoose
   .connect(process.env.MONGO_URI)
-
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Mongo Error:", err));
 
